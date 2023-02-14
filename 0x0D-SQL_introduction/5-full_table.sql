d29720fd82565fb88ce428d80b93d582a345a5e1
Michael Onoja <onoja16@gmail.com>
2023-02-14 14:36:30 +0100

    File updated

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
-- Prints the full description of the table first_table from the database
--- Query to print full description of a table
+-- print full description of the table first_table
+-- from the database hbtn_0c_0
 SHOW CREATE TABLE first_table;
